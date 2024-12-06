8e7abf1054c751e669bc066ed0fd0bc75c7e1de4
namhyunzi <hyunji3556@gmail.com>
2024-12-06 15:53:22 +0900

    폴더 생성

---
+++ bin/Source/PageView/naverProject000.js
@@ -0,0 +1,38 @@
+
+/**
+Constructor
+Do not call Function in Constructor.
+*/
+function naverProject000()
+{
+	AView.call(this);
+
+	//TODO:edit here
+
+}
+afc.extendsClass(naverProject000, AView);
+
+
+naverProject000.prototype.init = function(context, evtListener)
+{
+	AView.prototype.init.call(this, context, evtListener);
+
+	//TODO:edit here
+
+};
+
+naverProject000.prototype.onInitDone = function()
+{
+	AView.prototype.onInitDone.call(this);
+
+	//TODO:edit here
+
+};
+
+naverProject000.prototype.onActiveDone = function(isFirst)
+{
+	AView.prototype.onActiveDone.call(this, isFirst);
+
+	//TODO:edit here
+
+};
